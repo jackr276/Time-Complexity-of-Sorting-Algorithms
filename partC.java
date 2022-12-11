02f2705a275115fa7c3849454bbefd86161f39c0
jackr276 <robbins.jack@student.ccm.edu>
2022-12-10 20:23:49 -0500

    Made final touches

--- partC.java
+++ partC.java
@@ -1,12 +1,30 @@
+/**
+ * @author Jack Robbins
+ * CMP-233
+ * Professor Sawh
+ * Assignmet 4 part c
+ *
+ * The partC class is used to implement partC of assignment 4. Presents the user with 7 options about adding to,
+ * deleting from, and printing a binary search tree in 4 different ways.
+ */
+
 import java.util.Scanner;
 
+
 public class partC {
+    /**
+     * The main method that takes in user input and calls appropriate methods in the BinarySearchTree class
+     * as needed
+     * @param args not used
+     */
     public static void main(String[] args){
         displayMenu();
         BinarySearchTree bst = new BinarySearchTree();
         Scanner in = new Scanner(System.in);
+
         int userInput = Integer.parseInt(in.nextLine());
         while (userInput < 7 && userInput > 0){
+
             if (userInput == 1) {
                 // Perform an add operation
                 System.out.print("Enter a number to add: ");
@@ -28,7 +46,6 @@ public class partC {
                 // redisplay the menu
                 displayMenu();
                 userInput = Integer.parseInt(in.nextLine());
-
 
             } else if (userInput == 3){
                 // display breadth first
@@ -58,11 +75,13 @@ public class partC {
                 displayMenu();
                 userInput = Integer.parseInt(in.nextLine());
             }
-
         }
-
     }
 
+
+    /**
+     * A simple private helper function that displays the menu
+     */
     private static void displayMenu(){
         System.out.println("1.    Add an item");
         System.out.println("2.    Delete an item");
